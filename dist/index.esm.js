c7694d937053c93308f48bc7a2055fd685bd8eff
Aaron Ragudos <workemailaaronragudos@gmail.com>
2024-04-16 20:04:14 +0800

    add dist to repo

---
+++ dist/index.esm.js
@@ -0,0 +1,9 @@
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+function initializeToast() {
+}
+__name(initializeToast, "initializeToast");
+export {
+  initializeToast
+};
+//# sourceMappingURL=index.esm.js.map
